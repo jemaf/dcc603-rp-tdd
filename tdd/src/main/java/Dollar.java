2b735a71d8c0c782231f4c43b0e2d8876d1b70ee
Lucas Cassio Costa <lccosta2604@gmail.com>
2025-06-10 18:19:31 -0300

    superclass Money e outros testes

--- tdd/src/main/java/Dollar.java
+++ tdd/src/main/java/Dollar.java
@@ -1,5 +1,13 @@
 public class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}
-    void times(int multiplier) {}
-}
+    private int amount;
+    Dollar(int amount) {
+        this.amount= amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+    public boolean equals(Object object)  {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}	
