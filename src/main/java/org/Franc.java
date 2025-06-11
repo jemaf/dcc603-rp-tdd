29d2e9434a1c93c2fc49e9fbc0630ac03408772d
Lauraolv <oliveiraribeirolaura@icloud.com>
2025-06-11 15:45:21 -0300

    Refactors money logic and removes redundant test .

---
+++ src/main/java/org/Franc.java
@@ -0,0 +1,11 @@
+package org;
+
+class Franc extends Money {   				
+   Franc(int amount) {      
+      this.amount= amount;
+    }					
+    Franc times(int multiplier)  {      
+       return new Franc(amount * multiplier);					
+    }   
+   		
+}
