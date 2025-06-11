45b705db73a9e5f4c5ef537f910d5b4113ad061c
Lauraolv <oliveiraribeirolaura@icloud.com>
2025-06-11 15:52:48 -0300

    Refactors Money hierarchy for abstraction

--- src/main/java/org/Franc.java
+++ src/main/java/org/Franc.java
@@ -1,11 +1,12 @@
 package org;
 
-class Franc extends Money {   				
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
-   		
+class Franc extends Money {
+   Franc(int amount) {
+      this.amount = amount;
+   }
+
+   Money times(int multiplier) {
+      return new Franc(amount * multiplier);
+   }
+
 }
--- src/main/java/org/Money.java
+++ src/main/java/org/Money.java
@@ -1,9 +1,20 @@
 package org;
 
-public class Money {
-    protected int amount;
-    public boolean equals(Object object)  {
+abstract class Money {
+   protected int amount;
+
+   public boolean equals(Object object) {
       Money money = (Money) object;
-      return amount == money.amount;
-   }   
+      return amount == money.amount && getClass().equals(money.getClass());
+   }
+
+   static Money dollar(int amount) {
+      return new Dollar(amount);
+   }
+
+   static Money franc(int amount) {
+      return new Franc(amount);
+   }
+
+   abstract Money times(int multiplier);
 }
