073fe94859128da5cccc679fb9728a1d0cf2508b
Kou Kinoshita <kouichi.but.the.ichi.is.silent@gmail.com>
2025-06-14 19:43:29 -0300

    commit (3): Abstract times() method and Franc factory

--- src/main/java/br/com/kou/tp2/Franc.java
+++ src/main/java/br/com/kou/tp2/Franc.java
@@ -2,10 +2,12 @@ package br.com.kou.tp2;
 
 class Franc extends Money{
     private int amount;
+
     Franc(int amount) {
         this.amount= amount;
     }
-    Franc times(int multiplier)  {
+
+    Money times(int multiplier)  {
         return new Franc(amount * multiplier);
     }
 }
