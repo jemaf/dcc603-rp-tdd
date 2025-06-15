81724b9e7c1d4a78a68b36ebb963440fc4099438
brunasaturnino <brunasaturnino03@gmail.com>
2025-06-15 16:11:14 -0300

    third commit

--- src/main/java/org/Dollar.java
+++ src/main/java/org/Dollar.java
@@ -5,12 +5,8 @@ class Dollar extends Money {
     Dollar(int amount) {
         this.amount = amount;
     }
-
-    Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-    public boolean equals(Object object)  {
-       Dollar dollar = (Dollar) object;
-       return amount == dollar.amount;
-}
+
 }
--- src/main/java/org/Franc.java
+++ src/main/java/org/Franc.java
@@ -5,11 +5,8 @@ class Franc extends Money{
    Franc(int amount) {      
       this.amount= amount;
     }					
-    Franc times(int multiplier)  {      
+    Money times(int multiplier)  {      
        return new Franc(amount * multiplier);					
     }   
-    public boolean equals(Object object) {					
-       Franc franc = (Franc) object;      
-       return amount == franc.amount;					
-     }					
+				
 }
