4220c5796042e4002b04b257632dc493f1c2fc12
Carolina Pilla <carol.moyapilla@gmail.com>
2025-06-15 11:31:17 -0300

    (2)

--- Dollar.java
+++ Dollar.java
@@ -1,15 +1,48 @@
-class Dollar {
-    int amount = 10;
+class Money {
+    protected int amount;
 
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
+
+class Dollar extends Money {
     Dollar(int amount) {
+        this.amount = amount;
     }
 
-    void times(int multiplier) {
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
+
+class Franc extends Money {
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
     }
 }
 
 public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+}
+
+public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+}
+
+public void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
 }
