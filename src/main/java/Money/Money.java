54a1693a9b8a33ba7a37c94421a1e0df7daf6b23
davi <davi.araujo@onfly.com.br>
2025-06-16 17:49:00 -0300

    add currency

--- src/main/java/Money/Money.java
+++ src/main/java/Money/Money.java
@@ -1,10 +1,30 @@
 package Money;
 
-public class Money  {
+abstract public class Money {
     protected int amount;
+    protected String currency;
 
-    public boolean equals(Object object)  {
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount;
+    }
+
+    abstract Money times(int multiplier);
+
+    public String currency() {
+        return currency;
     }
 }
