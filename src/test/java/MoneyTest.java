0ddf856f469fd26a2aa22f814deae6fdafa63cc3
lukasara3 <lucaoamsilva@gmail.com>
2025-06-16 17:52:26 -0300

    Passo2: Adiciona Franco, perclasse Money e testes (green)

---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import java.beans.Transient;
+import org.junit.Test;
+
+public class MoneyTest {
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+
+    @Test 
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test 
+    public void testMultiplication() {
+      Dollar five = new Dollar(5);
+      assertEquals(new Dollar(10), five.times(2));
+      assertEquals(new Dollar(15), five.times(3));
+    }
+}
