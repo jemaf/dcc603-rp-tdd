f4154224bd562b19c30b40e69261ee5ce62e8a9a
Lucas Werlang <lucaswewe2806@gmail.com>
2025-06-16 23:28:38 -0300

    referencias alteradas em Money e remoção de Dollar e Franc

--- TP2_TDD/src/main/java/tp/Dollar.java
+++ TP2_TDD/src/main/java/tp/Dollar.java
@@ -1,18 +1,1 @@
-package tp;
 
-class Dollar extends Money{
-    private int amount;
-    private String currency;
-
-    Dollar(int amount, String currency)  {
-        super(amount, currency);
-    }
-
-    Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return "USD";
-    }
-}
--- TP2_TDD/src/main/java/tp/Franc.java
+++ TP2_TDD/src/main/java/tp/Franc.java
@@ -1,17 +1,1 @@
-package tp;
 
-class Franc extends Money{
-    private int amount;
-    private String currency;
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-    Money times(int multiplier)  {
-        return Money.franc(amount * multiplier);
-    }
-
-    String currency() {
-        return "CHF";
-    }
-}
