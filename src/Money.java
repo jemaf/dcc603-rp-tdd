8f6e0bb1c021f52e81f1f364e7b2fd02d571f894
Wesley Marques <wesleymarquesdc@gmail.com>
2025-06-17 17:54:31 -0300

    step 4

--- src/Money.java
+++ src/Money.java
@@ -1,18 +1,28 @@
 abstract class Money {
     protected int amount;
-    
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
 
-    static Money dollar(int amount) {
-        return new Dollar(amount);
+    static Money dollar(int amount)  {
+        return new Dollar(amount, "USD");
     }
-
+    
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 	
-    abstract Money times(int multiplier);  
+    abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
 }
