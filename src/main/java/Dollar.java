47e7ffe743ff04fa571b831f3cd3799d19c8082c
Vitor Terra Mattos <vtmpv2210@gmail.com>
2025-06-18 01:07:36 -0300

    Primeiro commit - Classes Dollar e DollarTest criadas

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,5 +1,7 @@
 public class Dollar {
+
     int amount = 10;
     Dollar(int amount) {}
     void times(int multiplier) {}
+
 }
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DollarTest {
     @Test
     public void testMultiplication() {
+
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
