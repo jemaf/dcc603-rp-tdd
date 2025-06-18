bff4fb62032e5d19dda71a06cb4942fec02a5256
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-06-18 12:03:53 -0300

    Turning Money into abstract class and also using factory

--- src/src/Money.java
+++ src/src/Money.java
@@ -1,8 +1,18 @@
-public class Money {
+abstract class Money {
     protected int amount;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    static Franc franc(int amount)  {
+        return new Franc(amount);
+    }
+
+    static Dollar dollar(int amount)  {
+        return new Dollar(amount);
+    }
+
+    abstract Money times(int multiplier);
 }
