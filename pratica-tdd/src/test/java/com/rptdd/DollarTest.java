9f161b2c00d6bd350230ccd5c8ca9a0a2f296ecb
Vitor Costa <vitorcostamgbr@gmail.com>
2025-06-18 16:03:28 -0300

    Adicionando a superclasse Money e a classe Franc + testes de equidade

--- pratica-tdd/src/test/java/com/rptdd/DollarTest.java
+++ pratica-tdd/src/test/java/com/rptdd/DollarTest.java
@@ -9,7 +9,7 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 }
