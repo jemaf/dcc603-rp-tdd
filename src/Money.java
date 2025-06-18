46f3612986284a6819968e702dc31170282cc564
Bruno Silva <brunosoaresbd@hotmail.com>
2025-06-18 04:09:00 -0300

    Quinto commit

--- src/Money.java
+++ src/Money.java
@@ -1,29 +1,30 @@
-abstract class Money {
+public class Money {
     protected int amount;
 
     protected String currency;
 
-    Money(int amount, String currency) {
+    public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier);
-
-    String currency() {
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+    public String currency() {
         return currency;
     }
 
-    static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 }
