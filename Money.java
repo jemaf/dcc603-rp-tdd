bfeeba631acc750ce2b4ae5de65a73d972ce22f0
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2025-06-19 20:28:58 -0300

    commit 4

--- Money.java
+++ Money.java
@@ -1,5 +1,23 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    static Money dollar(int amount)  {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
@@ -8,11 +26,4 @@ abstract class Money {
 
     abstract Money times(int multiplier);
 
-    static Money dollar(int amount)  {
-        return new Dollar(amount);
-    }
-
-    static Money franc(int amount) {
-        return new Franc(amount);
-    }
 }
