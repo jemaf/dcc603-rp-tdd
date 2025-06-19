36842ac4efea9b521d67b094a4a3a5916ee4cdf2
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2025-06-19 20:01:26 -0300

    commit 2

--- Dollar.java
+++ Dollar.java
@@ -1,10 +1,10 @@
-class Dollar {
-    int amount;
-    Dollar(int amount) {
-        this.amount= amount;
-    }
-    void times(int multiplier) {
-        amount= amount * multiplier;
-    }
-}		
+class Dollar extends Money {
 
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+}
