104cd1e19f87e87155eff81c6dbbdc1fa558c037
carlabferreira <carla.be.ferreira@gmail.com>
2025-06-19 15:47:48 -0300

    Quinto commit

--- src/Money.java
+++ src/Money.java
@@ -1,16 +1,30 @@
-abstract class Money {
-   private String currency; 
+class Money {
+    protected int amount;
+    private String currency; 
 
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
-   }
+    String currency() {
+        return currency;
+    }
 
-   static Money franc(int amount) {
-      return new Franc(amount, "CHF");
-   }
+    static Money dollar(int amount)  {
+        return new Money(amount, "USD");
+    }
 
-   Money(int amount, String currency) {
-      this.amount = amount;
-      this.currency = currency;
-   }
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+        
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+        
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 }
