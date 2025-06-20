3fa0f5528e144828f19aade4e78c2c0404f312ed
lucaswnn <lucaswnn@hotmail.com>
2025-06-20 16:28:19 -0300

    commit 4

--- src/main/java/RP2/Money.java
+++ src/main/java/RP2/Money.java
@@ -2,19 +2,29 @@ package RP2;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
-
-    static Dollar dollar(int amount) {
-        return new Dollar(amount);
-    }
-
-    static Money franc(int amount) {
-        return new Franc(amount);
-    }
-
-    abstract Money times(int multiplier);
 }
