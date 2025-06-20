192e3c6f31160785646a403933a02ced3bba3392
gomidegui <guigomide20@gmail.com>
2025-06-20 02:36:01 -0300

    Passo seguinte até 1.2.1

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,5 +1,17 @@
 public class Dollar {
-    int amount = 10;
-    Dollar(int amount) { }
-    void times(int multiplier) { }
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
