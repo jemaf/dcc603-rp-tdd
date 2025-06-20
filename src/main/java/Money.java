eda609f67499d323b603cf4c6d17aeb08250cd38
Pedro Henrique de Mattos Gomes <pedromattosg@hotmail.com>
2025-06-20 00:35:27 -0300

    Feat: Money como superclasse

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,11 +1,21 @@
 package main.java;
 
-public class Money {
+public abstract class Money {
    protected int amount;
     
+   public static Dollar dollar(int amount) {
+      return new Dollar(amount);
+   }
+
+   public static Money franc(int amount) {
+      return new Franc(amount);
+   }
+
+   public abstract Money times(int multiplier);
+
    public boolean equals(Object object)  {
       Money money = (Money) object;
-      return amount == money.amount;
+      return amount == money.amount && getClass().equals(money.getClass());
    }   
-   
+
 }
