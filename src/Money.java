05eb3aa79b82e77feee056fe2bb1218863126122
Eduardo-Klausing <eduardokgmuniz@gmail.com>
2025-06-20 00:19:06 -0300

    commit 5

--- src/Money.java
+++ src/Money.java
@@ -1,26 +1,27 @@
 abstract class Money  {
    protected String currency;
 
+    static Money dollar(int amount)  {
+      return new Money(amount, "USD");
+   }
+
+    static Money franc(int amount) {
+      return new Money(amount, "CHF");
+    }  
+
     Money(int amount, String currency) {
       this.amount = amount;
       this.currency = currency;
     }
 
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
-   }
-   
    public boolean equals(Object object) {
       Money money = (Money) object;
       return amount == money.amount && getClass().equals(money.getClass());
    }
 
-   abstract Money times(int multiplier);
-
-    static Money franc(int amount) {
-      return new Franc(amount, "CHF");
-    }  
-
+    Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
+   }
     String currency(){
         return currency;
     }
