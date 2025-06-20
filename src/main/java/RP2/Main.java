4d3793717a0a4881b6c11ee802e115592bafff7d
lucaswnn <lucaswnn@hotmail.com>
2025-06-20 15:48:32 -0300

    commit 1

---
+++ src/main/java/RP2/Main.java
@@ -0,0 +1,12 @@
+package RP2;
+
+import RP2.Tests;
+
+public class Main {
+    public static void main(String[] args) {
+        Tests t = new Tests();
+        t.testMultiplication();
+        System.out.println("Hello World!");
+
+    }
+}
