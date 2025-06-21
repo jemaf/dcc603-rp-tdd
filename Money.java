5d5750158475dbe07cfaadaaa4ca7c4222e4ff06
Juan Coelho <juancoelho25@hotmail.com>
2025-06-21 15:28:06 -0300

    Commit5

--- Money.java
+++ Money.java
@@ -1,29 +1,30 @@
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
-    static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
-
     String currency() {
         return currency;
     }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
 }
--- MoneyTest.java
+++ MoneyTest.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MoneyTest {
 
     @Test
-    public void testDollarMultiplication() {
+    public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
