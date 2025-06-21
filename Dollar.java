4f587f368143151b04e40dcbdb7b2b58c6908941
riquelme <riquelme3m@outlook.com>
2025-06-21 14:45:39 -0300

    Item 1 done

---
+++ Dollar.java
@@ -0,0 +1,13 @@
+package Test_Driven_Development_Practical_Assignemnt_UFMG;
+
+public class Dollar {
+    int amount = 10;
+    
+    public Dollar(int amount) {
+       
+    }
+    
+    void times(int multiplier) {
+        
+    }
+}
