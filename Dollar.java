0b4bff26941ff5accb2710f803bf3ca88cedd612
Juan Coelho <juancoelho25@hotmail.com>
2025-06-21 15:17:43 -0300

    Commit2

--- Dollar.java
+++ Dollar.java
@@ -1,9 +1,10 @@
-class Dollar {
-    int amount;
+class Dollar extends Money {
+
     Dollar(int amount) {
         this.amount= amount;
     }
-    void times(int multiplier) {
-        amount= amount * multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
-}		
+
+}
