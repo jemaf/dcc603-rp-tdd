3924a2647f7399dc6545e310908d94da9f3adf75
riquelme <riquelme3m@outlook.com>
2025-06-21 16:33:19 -0300

    Done

--- Money.java
+++ Money.java
@@ -1,6 +1,6 @@
 package Test_Driven_Development_Practical_Assignemnt_UFMG;
 
-abstract class Money  {
+class Money  {
    protected int amount;
    protected String currency;
 
@@ -14,12 +14,14 @@ abstract class Money  {
       return amount == money.amount && getClass().equals(money.getClass());
    }
     static Money dollar(int amount)  {
-      return new Dollar(amount,"USD");
+      return new Money(amount,"USD");
    }
     static Money franc(int amount)  {
-      return new Franc(amount,"CHF");
+      return new Money(amount,"CHF");
    }
-   abstract Money times(int multiplier); 
+   Money times(int multiplier){
+    return new Money(amount*multiplier,currency);
+   }
    String currency(){
         return currency;
    }
