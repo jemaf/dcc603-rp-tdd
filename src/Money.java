a75e393d2ee2b3cb155bad0a80de7305894f1c24
Pedro Henrique <pedrohfernandes2001@gmail.com>
2025-06-21 15:49:01 -0300

    Commit 5 - Exclusão das classes Dollar e Franc

--- src/Money.java
+++ src/Money.java
@@ -1,20 +1,20 @@
 package src;
 
-public abstract class Money {
-    protected int amount;
-    protected String currency;
+public class Money {
+    private int amount;
+    private String currency;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public Money(int amount, String currency) {
@@ -26,5 +26,7 @@ public abstract class Money {
         return currency;
     }
 
-    public abstract Money times(int multiplier);
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 }
