c1d9a4c94cd6bdff50a074a5203c802c4a8b4a78
Juan Coelho <juancoelho25@hotmail.com>
2025-06-21 15:25:24 -0300

    Commit4

--- Money.java
+++ Money.java
@@ -1,18 +1,29 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
 
-    public boolean equals(Object object)  {
+    static Money dollar(int amount)  {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
 
     abstract Money times(int multiplier);
 
-    static Money dollar(int amount)  {
-        return new Dollar(amount);
-    }
-
-    static Money franc(int amount) {
-        return new Franc(amount);
-    }
 }
