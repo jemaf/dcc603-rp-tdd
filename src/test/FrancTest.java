c7c6bfb6cd28ab75263cf6f82f6a068140a88a59
Gabriele <gabrielepinheirosa@gmail.com>
2025-06-22 17:18:07 -0300

    Novos métodos, testes e classes Franc e Money

---
+++ src/test/FrancTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FrancTest {
+	
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
+}
