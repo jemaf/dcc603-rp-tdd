62ef4b185450bc6eaec1e653529ac3ef929025a8
Jonas F. Lopes <jonas.lopes@uemg.br>
2025-06-22 19:21:18 -0300

    Adiciona a classe Money e atualiza subclasses dela

---
+++ src/test/java/FrancTest.java
@@ -0,0 +1,13 @@
+
+public class FrancTest {
+    public void testMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    public void testEquality() {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
