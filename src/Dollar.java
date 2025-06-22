ad822ccd78c262dea5f503810375707b18beb75c
rafaelchimicatti <rafaelchimicatti@hotmail.com>
2025-06-22 14:43:03 -0300

    commit 2

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,11 +1,25 @@
 class Dollar {
-   int amount = 10;
-   Dollar(int amount) {}			
-   void times(int multiplier) {}
+    private int amount;
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
+   }
 }	
 
 public void testMultiplication() {
    Dollar five = new Dollar(5);
-   five.times(2);
-   assertEquals(10, five.amount);
+   assertEquals(new Dollar(10), five.times(2));
+   assertEquals(new Dollar(15), five.times(3));
+}
+
+public void testEquality() {
+   assertTrue(new Dollar(5).equals(new Dollar(5)));
+   assertFalse(new Dollar(5).equals(new Dollar(6)));
+}
+
+public boolean equals(Object object)  {
+   Dollar dollar = (Dollar) object;
+   return amount == dollar.amount;
 }
