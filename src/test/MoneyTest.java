2e23d06a63e0be8d1c9f30567059ee6390a529a4
Gabriele <gabrielepinheirosa@gmail.com>
2025-06-22 18:55:23 -0300

    Novo teste testDifferentClassEquality e remoção das classes Dollar e Franc

--- src/test/MoneyTest.java
+++ src/test/MoneyTest.java
@@ -1,10 +1,23 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MoneyTest {
+	
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+	}
 	
 	public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}	
 	
+	public void testEquality() {
+		assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}	
 }
