eae348762a81f6269e21473bf42ffe41b04ad05f
Arturfsouza <artur.fonseca.souza@gmail.com>
2025-06-22 13:58:56 -0300

    commit5

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,29 +1,37 @@
-abstract class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
-    static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
-    Money(int amount, String currency) {
+    public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier); 
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
-    String currency() {
+    public String currency() {
         return currency;
     }
 
+    @Override
     public boolean equals(Object object) {
+        if (!(object instanceof Money)) return false;
         Money money = (Money) object;
-        return amount == money.amount &&
-               currency.equals(money.currency());
+        return amount == money.amount && currency.equals(money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
