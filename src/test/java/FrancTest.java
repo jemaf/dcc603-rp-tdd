2eef041c74603a0bce71ffc4a59fda4ae927684d
Jonas F. Lopes <jonas.lopes@uemg.br>
2025-06-22 19:32:35 -0300

    Adiciona suporte a fábrica de moedas e testes

--- src/test/java/FrancTest.java
+++ src/test/java/FrancTest.java
@@ -9,5 +9,6 @@ public class FrancTest {
     public void testEquality() {
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
