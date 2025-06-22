44535d08efaef2d2f00ba429418977d12f74351f
Bedro Boseja <104467351+Pedro-Boseja@users.noreply.github.com>
2025-06-22 12:39:12 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,37 +1,51 @@
 abstract class Money {
     protected int amount;
+    private String currency;
     public boolean equals(Object object) {
       Money money = (Money) object;
       return amount == money.amount && getClass().equals(money.getClass());
    }
 
-   static Dollar dollar(int amount) {
-      return new Dollar(amount);
+   static Money dollar(int amount) {
+      return new Dollar(amount, "USD");
    }
 
    static Money franc(int amount) {
-      return new Franc(amount);
+      return new Franc(amount, "CHF");
     }
 
+    Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
+
    abstract Money times(int multiplier);
+
+   abstract String currency();
 }
 
 class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier)  {
-      return new Dollar(amount * multiplier);
+      return Money.dollar(amount * multiplier);
    }	
+//    String currency() {
+//        return currency;
+//     }
 }
 
 class Franc extends Money {
-    Franc(int amount) {
-        this.amount = amount;
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier)  {
-      return new Franc(amount * multiplier);
+      return Money.franc(amount * multiplier);
    }
+//     String currency() {
+//       return currency;
+//    }
 }
 
 public void testMultiplication() {
@@ -52,4 +66,9 @@ public void testEquality() {
    assertTrue(Money.franc(5).equals(Money.franc(5)));
    assertFalse(Money.franc(5).equals(Money.franc(6)));
    assertFalse(Money.franc(5).equals(Money.dollar(5)));
+}
+
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
 }
