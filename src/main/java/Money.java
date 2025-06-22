8ceea37809a76a6e70aa90450a53b16cab3dd74b
Thalys Barbosa <thalysvinicius23@gmail.com>
2025-06-22 19:38:27 -0300

    Implement Dollar and Franc classes with multiplication and equality tests

---
+++ src/main/java/Money.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Money {
+   protected int amount;
+
+   public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return amount == money.amount;
+   }   
+
+}
