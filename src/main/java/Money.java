aec39fd60d5c75c6f2795f9feabab1efe31b6fd4
Thalys Barbosa <thalysvinicius23@gmail.com>
2025-06-22 19:52:35 -0300

    Refactor Dollar and Franc classes to include currency in constructors and update multiplication methods; add currency tests

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,20 +2,29 @@ package main.java;
 
 public abstract class Money {
    protected int amount;
+   protected String currency;
 
- public static Dollar dollar(int amount) {
-      return new Dollar(amount);
+   public Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
    }
 
-   public static Money franc(int amount) {
-      return new Franc(amount);
+   public static Money dollar(int amount) {
+      return new Dollar(amount, "USD");
    }
 
-   public abstract Money times(int multiplier);
+     public static Money franc(int amount) {
+      return new Franc(amount, "CHF");
+   }
 
    public boolean equals(Object object)  {
       Money money = (Money) object;
       return amount == money.amount && getClass().equals(money.getClass());
    }   
 
+   public abstract Money times(int multiplier);
+
+   public String currency(){
+      return currency;
+   };
 }
