a1c1a9b651a2d1f3adad8c30a3ccf6ab9d21854c
Guilherme Novais <guilhermens@ufmg.com>
2025-06-22 15:29:02 -0300

    feat(Money): add currency support to Dollar and Franc classes

--- Dollar.java
+++ Dollar.java
@@ -1,70 +1,44 @@
-abstract class Money  {
+class Money  {
   protected int amount;
   protected String currency;
-
-  public boolean equals(Object object) {
-    Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
-  }
-
-  static Dollar dollar(int amount)  {
-    return new Dollar(amount, "USD");
-  }
-
-  static Money franc(int amount) {
-    return new Franc(amount, "CHF");
-  }
-
-  abstract Money times(int multiplier);
-
+  
   String currency() {
     return currency;
   };
 
-  Money(int amount, String currency) {
-    this.amount = amount;
-    this.currency = currency;
-  }
-}
-
-class Dollar extends Money {
-  private int amount;
-  Dollar(int amount, String currency) {
-    super(amount, currency);
-  }
-  Money times(int multiplier)  {
-    return Money.dollar(amount * multiplier);
-  }
-}
-
-class Franc extends Money {
-  private int amount;
-  Franc(int amount, String currency) {
-    super(amount, currency);
-  }
-  Money times(int multiplier)  {
-    return Money.franc(amount * multiplier);
-  }
+  static Money dollar(int amount)  {
+      return new Money(amount, "USD");
+   }
+	
+   static Money franc(int amount) {
+      return new Money(amount, "CHF");
+   }
+    
+   Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
+	
+   public boolean equals(Object object) {
+      Money money = (Money) object;
+      return amount == money.amount && currency().equals(money.currency());
+   }
+	
+   Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
+   }
 }
 
 public void testEquality() {
-  assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-  assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-  assertTrue(Money.franc(5).equals(Money.franc(5)));
-  assertFalse(Money.franc(5).equals(Money.franc(6)));
-  assertFalse(Money.franc(5).equals(Money.dollar(5)));
-}
+   assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+   assertFalse(Money.franc(5).equals(Money.dollar(5)));
+}	
 
 public void testMultiplication() {
   Money five = Money.dollar(5);
   assertEquals(Money.dollar(10), five.times(2));
   assertEquals(Money.dollar(15), five.times(3));
-}
-
-public void testFrancMultiplication() {
-  Money five = Money.franc(5);
-  assertEquals(Money.franc(10), five.times(2));
-  assertEquals(Money.franc(15), five.times(3));
 }
 
 public void testCurrency() {
