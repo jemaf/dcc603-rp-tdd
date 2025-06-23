9ee55417e3ea345d2ce68318da87d17d937a67a6
pollybrandao <pollyannybrandao@gmail.com>
2025-06-22 22:08:27 -0300

    Criação de pastas para organização; Criação de novas classes e testes; Alteração da classe Dollar e DollarTest

---
+++ src/class/Dollar.java
@@ -0,0 +1,9 @@
+class Dollar extends Money{
+    private int amount;
+
+    Dollar(int amount) { this.amount= amount;}
+
+    Dollar times(int multiplier) { 
+        return new Dollar(amount * multiplier);
+    }
+}
