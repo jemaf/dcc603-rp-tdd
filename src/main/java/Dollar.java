7f6615abd3eaac7859c59b678185a8790b5bd2ef
André Luiz Alves <andrelac963@gmail.com>
2025-06-22 22:46:41 -0300

    feat: adiciona método equals à classe Dollar e testa igualdade entre objetos

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -4,8 +4,14 @@ class Dollar {
     Dollar(int amount) {
         this.amount = amount;
     }
-    
+
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -8,4 +8,10 @@ public class MoneyTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
 }
