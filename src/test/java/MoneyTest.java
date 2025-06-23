ecec73bf3730b0ff21247944f6137062048b851f
André Luiz Alves <andrelac963@gmail.com>
2025-06-22 22:45:41 -0300

    test: refatora o teste de multiplicação para melhorar legibilidade

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -2,12 +2,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MoneyTest {
- @Test
+    @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 }
