b323ff784f6c0fb91ae250927d13605f3a65d688
André Luiz Alves <andrelac963@gmail.com>
2025-06-22 22:49:39 -0300

    refactor: cria superclasse Money e move lógica comum de Dollar e Franc para ela

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -21,4 +21,12 @@ public class MoneyTest {
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
     }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
 }
