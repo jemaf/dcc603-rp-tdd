898e6f93d53968e57d9283bb16ee71443342dfea
pollybrandao <pollyannybrandao@gmail.com>
2025-06-22 22:13:23 -0300

    Alteração nas classes; Função times em Money; Factory de Franc;

--- src/class/Franc.java
+++ src/class/Franc.java
@@ -1,9 +1,9 @@
 class Franc extends Money{
     private int amount;
-    
+
     Franc(int amount) {this.amount= amount;}
 
-    Franc times(int multiplier)  {
+    Money times(int multiplier)  {
         return new Franc(amount * multiplier);
     }
 }
