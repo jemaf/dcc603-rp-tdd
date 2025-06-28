f555dc9a3bdbe8d27680b833c5951b392a1780de
leticiaacs <leticiaalexandre2020@gmail.com>
2025-06-28 14:10:26 -0300

    Correção de erros do primeiro commit

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,5 +1,10 @@
-public class Dollar {
-    int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
+class Dollar {
+   int amount = 10;
+   Dollar(int amount) {}			
+   void times(int multiplier) {}
+}	
+public void testMultiplication() {
+   Dollar five = new Dollar(5);
+   five.times(2);
+   assertEquals(10, five.amount);
 }
--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-
-@Test
-public class Test {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-}
