6ed664ac92a5e798bd1f3858b2570329823ea170
leticiaacs <leticiaalexandre2020@gmail.com>
2025-06-28 14:33:37 -0300

    Novas modificações de classes

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -4,34 +4,54 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 abstract class Money {
    protected int amount;
+   protected String currency;
+   Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
+
+   static Money dollar(int amount)  {
+      return new Dollar(amount, "USD");
+   }
+
+   static Money franc(int amount) {
+      return new Franc(amount, "CHF");
+   }
+
+   String currency() {
+      return currency;
+   }
    
    public boolean equals(Object object) {
       Money money = (Money) object;
       return amount == money.amount && getClass().equals(money.getClass());
    }
 
-   static Dollar dollar(int amount)  {
-      return new Dollar(amount);
-   }
-
-   static Money franc(int amount) {
-      return new Franc(amount);
-   }
 
    abstract Money times(int multiplier);  
 }
 
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
+}
+
 class Dollar extends Money {
-   Dollar(int amount) {
-      this.amount= amount;
+   private String currency;
+
+   Dollar(int amount, String currency)  {
+      super(amount, currency);
    }
    Money times(int multiplier)  {
-      return new Dollar(amount * multiplier);
+      return Money.dollar(amount * multiplier);
    }
    public boolean equals(Object object)  {
    Dollar dollar = (Dollar) object;
    return amount == dollar.amount;
 }
+    String currency() {
+    return "USD";
+    }
 }	
 public void testMultiplication() {
    Money five = Money.dollar(5);
@@ -41,16 +61,21 @@ public void testMultiplication() {
 
 
 class Franc extends Money {   				
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
+   private String currency;
+	
+   Franc(int amount, String currency) {
+      super(amount, currency);
+   }	
    Money times(int multiplier)  {
-      return new Franc(amount * multiplier);
+      return Money.franc(amount * multiplier);
    }		  
     public boolean equals(Object object) {					
        Franc franc = (Franc) object;      
        return amount == franc.amount;					
      }					
+      String currency() {
+      return "CHF";
+      }
 }
 
 public void testFrancMultiplication() {
